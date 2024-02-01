9cd2e6478a699041439aba3627624e3f0c3dfb0a
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-02-01 18:36:41 +0300

    fix for based on comments Daniil Yurevich

--- s21_memcmp.c
+++ s21_memcmp.c
@@ -1,24 +1,18 @@
 #include "s21_string.h"
 
+//Функция принимает на вход 2 строки и количество байтов этих строк
+//возвращая результат сравнивнения первые n байтов строк.
+
 int s21_memcmp(const void *str1, const void *str2, size_t n) {
-    int return_val = 0;
+    if (!n) {
+        return 0;
+    }
     if (str1 && str2) {
         for (size_t i = 0; i < n; i++) {
-            char symb1 = *((char*) (str1 + i));
-            char symb2 = *((char*) (str2 + i));
-            if (symb1 - symb2) {
-                return_val = symb1 - symb2;
-                break;
+            if (*((char*) (str1 + i)) != *((char*) (str2 + i))) {
+                return *((char*) (str1 + i)) - *((char*) (str2 + i));
             }
         }
     }
-    return return_val;
+    return 0;
 }
-
-//TESTS
-
-// if (memcmp ("foo", "foobar", 2) == 0) puts("SUCCESS");
-// if (memcmp ("foo", "foobar", 3) == 0) puts("SUCCESS");
-// if (memcmp ("foo", "foobar", 4) != 0) puts("SUCCESS");
-// if (memcmp ("foo", "bar", 1) != 0) puts("SUCCESS");
-// if (memcmp ("foo", "bar", 3) != 0) puts("SUCCESS");
