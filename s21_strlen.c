b80293a7676388c163d96471a332c54d996008b4
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-02-02 15:06:32 +0300

    update my previous push

--- s21_strlen.c
+++ s21_strlen.c
@@ -1,7 +1,8 @@
-// size_t strlen(const char *str)
-// Вычисляет длину строки str до завершающего нулевого символа, но не включая его.
-
 #include "s21_string.h"
+
+// size_t strlen(const char *str) 
+// принимает на вход адрес строки
+// вычисляет длину строки str до завершающего нулевого символа, но не включая его.
 
 size_t s21_strlen(const char *str) {
     size_t n = 0;
--- s21_strncat.c
+++ s21_strncat.c
@@ -1,7 +1,8 @@
-// char *strncat(char *dest, const char *src, size_t n)	
-// Добавляет строку, на которую указывает src, к концу строки, на которую указывает dest, длиной до n символов.
-
 #include "s21_string.h"
+
+// char *strncat(char *dest, const char *src, size_t n)
+// принимает нп вход адреса на 2 строки и количество символов которое будем добалять
+// добавляет строку, на которую указывает src, к концу строки, на которую указывает dest, длиной до n символов.
 
 char *s21_strncat(char *dest, const char *src, size_t n) {
     if (n > 0 && src) {
