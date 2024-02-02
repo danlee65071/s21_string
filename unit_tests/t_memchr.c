01fb70c1e3addef5be87d29311f5b22f7a0dadc5
Everette Lavona <everettl@mi-g4.msk.21-school.ru>
2024-02-02 12:40:11 +0300

    test

--- unit_tests/t_memchr.c
+++ unit_tests/t_memchr.c
@@ -10,7 +10,57 @@ START_TEST(empty_test) {
 }
 END_TEST
 
-// тестовый набор Suite, который может содержать несколько групп тестов TCase.
+START_TEST(zero_test) {
+  char str[15] = "endofstring";
+  int find_byte = '\0';
+  size_t num_byte = 12;
+
+  ck_assert_ptr_eq(my_memchr(str, find_byte, num_byte),
+                   memchr(str, find_byte, num_byte));
+}
+END_TEST
+
+START_TEST(beginning_test) {
+  char str[10] = "Start";
+  int find_byte = 'S';
+  size_t num_byte = 6;
+
+  ck_assert_ptr_eq(my_memchr(str, find_byte, num_byte),
+                   memchr(str, find_byte, num_byte));
+}
+END_TEST
+
+START_TEST(middle_test) {
+  char str[15] = "abrakAdabra";
+  int find_byte = 'A';
+  size_t num_byte = 12;
+
+  ck_assert_ptr_eq(my_memchr(str, find_byte, num_byte),
+                   memchr(str, find_byte, num_byte));
+}
+END_TEST
+
+START_TEST(num_mid_test) {
+  char str[15] = "abra217abra";
+  int find_byte = '1';
+  size_t num_byte = 12;
+
+  ck_assert_ptr_eq(my_memchr(str, find_byte, num_byte),
+                   memchr(str, find_byte, num_byte));
+}
+END_TEST
+
+START_TEST(num_arr_test) {
+  char str[] = {1, 3, 5, 8, 99, 7};
+  int find_byte = 99;
+  size_t num_byte = sizeof(int) * 6;
+
+  ck_assert_ptr_eq(my_memchr(str, find_byte, num_byte),
+                   memchr(str, find_byte, num_byte));
+}
+END_TEST
+// тестовый набор Suite, который может содержать несколько групп тестов
+// TCase.
 Suite *memset_suite(void) {
   Suite *suite = suite_create("memchr_suite");
   // Набор разбивается на группы тестов, разделённых по каким-либо критериям.
@@ -18,6 +68,11 @@ Suite *memset_suite(void) {
 
   // Добавление теста в группу тестов.
   tcase_add_test(tcase_core, empty_test);
+  tcase_add_test(tcase_core, zero_test);
+  tcase_add_test(tcase_core, beginning_test);
+  tcase_add_test(tcase_core, middle_test);
+  tcase_add_test(tcase_core, num_mid_test);
+  tcase_add_test(tcase_core, num_arr_test);
 
   // Добавление теста в тестовый набор.
   suite_add_tcase(suite, tcase_core);
--- unit_tests/t_memset.c
+++ unit_tests/t_memset.c
@@ -1,17 +1,69 @@
 #include "tests.h"
 
 START_TEST(abrakadabra_test) {
-  char beg[] = "abrakadabra";
+  char def[] = "abrakadabra";
   char expect[] = "abrakadabra";
   char repl = 'i';
   size_t num_byte = 12;
 
-  my_memset(beg, repl, num_byte);
+  my_memset(def, repl, num_byte);
   memset(expect, repl, num_byte);
-  ck_assert_ptr_eq(beg, expect);
+
+  ck_assert_ptr_eq(def, expect);
 }
 END_TEST
 
+START_TEST(empty_test) {
+  char def[] = "";
+  char expect[] = "";
+  char repl = 'i';
+  size_t num_byte = 0;
+
+  my_memset(def, repl, num_byte);
+  memset(expect, repl, num_byte);
+
+  ck_assert_ptr_eq(def, expect);
+}
+END_TEST
+
+START_TEST(zero_test) {
+  char def[] = "abrakadabra";
+  char expect[] = "abrakadabra";
+  char repl = 'i';
+  size_t num_byte = 0;
+
+  my_memset(def, repl, num_byte);
+  memset(expect, repl, num_byte);
+
+  ck_assert_ptr_eq(def, expect);
+}
+END_TEST
+
+START_TEST(uppercase_test) {
+  char def[] = "abrakadabra";
+  char expect[] = "abrakadabra";
+  char repl = 'I';
+  size_t num_byte = 5;
+
+  my_memset(def, repl, num_byte);
+  memset(expect, repl, num_byte);
+
+  ck_assert_ptr_eq(def, expect);
+}
+END_TEST
+
+START_TEST(num_test) {
+  char def[] = "abrakadabra";
+  char expect[] = "abrakadabra";
+  char repl = '4';
+  size_t num_byte = 12;
+
+  my_memset(def, repl, num_byte);
+  memset(expect, repl, num_byte);
+
+  ck_assert_ptr_eq(def, expect);
+}
+END_TEST
 // тестовый набор Suite, который может содержать несколько групп тестов TCase.
 Suite *memset_suite(void) {
   Suite *suite = suite_create("memset_suite");
@@ -20,6 +72,10 @@ Suite *memset_suite(void) {
 
   // Добавление теста в группу тестов.
   tcase_add_test(tcase_core, abrakadabra_test);
+  tcase_add_test(tcase_core, empty_test);
+  tcase_add_test(tcase_core, zero_test);
+  tcase_add_test(tcase_core, uppercase_test);
+  tcase_add_test(tcase_core, num_test);
 
   // Добавление теста в тестовый набор.
   suite_add_tcase(suite, tcase_core);
