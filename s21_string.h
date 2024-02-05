b6eb34e364287571981e62d03803fd2eff8e8a25
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-02-05 12:13:21 +0300

    trim func add && tests

--- s21_string.h
+++ s21_string.h
@@ -11,5 +11,6 @@ char *s21_strncat(char *dest, const char *src, size_t n);
 size_t s21_strlen(const char *str);
 int s21_strncmp(const char *str1, const char *str2, size_t n);
 char *s21_strncpy(char *dest, const char *src, size_t n);
+void *s21_trim(const char *src, const char *trim_chars);
 
 #endif
--- unit_tests/unit_tests.c
+++ unit_tests/unit_tests.c
@@ -9,7 +9,7 @@ void case_test(Suite *s, int *fail) {
 }
 
 int main() {
-  int numTests = 7;
+  int numTests = 8;
   int result[numTests];
   int fail = 0;
 
@@ -20,6 +20,7 @@ int main() {
   case_test(s21_strncat_test(), result);
   case_test(s21_strncmp_test(), result);
   case_test(s21_strncpy_test(), result);
+  case_test(s21_trim_test(), result);
 
   for (int i = 0; i < numTests; i++) {
     if (result[i] != 0) {
