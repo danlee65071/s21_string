4d163f57c8780e944d490fb615ebc96eb7a3e03e
DanLee <danlee65071@gmail.com>
2024-02-08 00:57:35 +0300

    fix

--- s21_insert.c
+++ s21_insert.c
@@ -4,35 +4,48 @@
 // в указанную позицию (start_index) в данной строке (src). 
 // В случае какой-либо ошибки следует вернуть значение NULL.
 
-void additionFunc(const char* fstStr, const char* secStr, size_t idx, char* resultStr) {
+// void additionFunc(const char* fstStr, const char* secStr, size_t idx, char* resultStr) {
+//     size_t i = 0;
+//     size_t j = 0;
+//     while (*(fstStr + j) != '\0') {
+//         *(resultStr + i) = *(fstStr + j);
+//         if (i == idx) {
+//             j = 0;
+//             while (*(secStr + j) != '\0') {
+//                 *(resultStr + i) = *(secStr + j);
+//                 i++;
+//                 j++;
+//             }
+//             j = i - j - 1;
+//             i--;
+//         }
+//         i++;
+//         j++;       
+//     }
+//     *(resultStr + i) = '\0';
+// }
+
+void additionFunc(const char* fstStr, const char* secStr, size_t idx, char** resultStr)
+{
     size_t i = 0;
     size_t j = 0;
-    while (*(fstStr + j) != '\0') {
-        *(resultStr + i) = *(fstStr + j);
-        if (i == idx) {
-            j = 0;
-            while (*(secStr + j) != '\0') {
-                *(resultStr + i) = *(secStr + j);
-                i++;
-                j++;
-            }
-            j = i - j - 1;
-            i--;
+    while (fstStr[j] != '\0')
+    {
+        if (j == idx) {
+            for (size_t k = 0; k < s21_strlen(secStr); k++)
+                (*resultStr)[i++] = secStr[k];
         }
-        i++;
-        j++;       
+        else
+            (*resultStr)[i++] = fstStr[j++];
     }
-    *(resultStr + i) = '\0';
+    (*resultStr)[i] = '\0';
 }
 
-void *s21_insert(const char *src, const char *str, size_t start_index) {
+
+void *s21_insert(const char *src, const char *str, size_t start_index)
+{
     char* resultStr = s21_NULL;
-    if (src && str && (resultStr = malloc(s21_strlen(src) + s21_strlen(str) * sizeof(char)))) {
-        if (start_index > 0) {
-            additionFunc(src, str, start_index, resultStr);
-        } else {
-            additionFunc(str, src, start_index, resultStr);
-        }
-    }
+    if (src && str && (resultStr = malloc(s21_strlen(src) + s21_strlen(str) * sizeof(char))))
+        additionFunc(src, str, start_index, &resultStr);
     return resultStr;
 }
