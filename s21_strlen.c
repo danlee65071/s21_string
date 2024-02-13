1ef1d994f658870b4f21fbd7d99ae351236eea6f
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-02-13 15:34:57 +0300

    update

--- s21_strlen.c
+++ s21_strlen.c
@@ -5,5 +5,5 @@
 // вычисляет длину строки str до завершающего нулевого символа, но не включая его.
 
 size_t s21_strlen(const char *str) {
-    return (s21_strchr(str, 0) - str);
+    return (int)(sizeof(str) - 1);
 }
