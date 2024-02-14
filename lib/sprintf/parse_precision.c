84a8bdea5183cba37a30bc8fa5e60eadca1f9549
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-02-14 18:14:01 +0300

    update

--- lib/sprintf/parse_precision.c
+++ lib/sprintf/parse_precision.c
@@ -1,13 +1,29 @@
 #include "s21_string.h"
 
 void parse_precision(const char *format, va_list arglist, t_sprintf* sprintf_args, int* i) {
-    while ((isdigit(*(format + i))) || (*(format + i) == '*') || (*(format + i) == '\0')) {
-        if (isdigit(*(format + i))) {
-            arglist->precision = sprintf_args[0];
-            (*i)++;
-            break;
-        }
-        arglist->precision = (int)(*(format + i) - 48);
+    if (*(format + i) == "*") {
+        sprintf_args->width = va_arg(arglist, int);
         (*i)++;
+    int buf_size = 1024;
+    char *buf = calloc(1024 * sizeof(char));
+
+    if (buf == NULL) {
+        return;
+    }
+    int idx = 0;
+    while ((isdigit(*(format + i))) || (*(format + i) == '\0')) {
+        buf[idx] = (int)(*(format + i) - 48);
+        (*i)++;
+        index++;
+        if (index >= buf_size) {
+            buf = realloc(buf, buf_size * 2);
+            if (buf == NULL) {
+                free(buf);
+                return;
+            }
+            buf_size *= 2;
+        }
+        sprintf_args->width = s21_atoi(buf);
+        free(buf);
     }   
 }
