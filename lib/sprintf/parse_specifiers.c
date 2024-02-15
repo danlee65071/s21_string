11f203fc27bb88d9837949eb33a5eebc6d3306d5
unknown <you@example.com>
2024-02-15 15:33:55 +0300

    вроде то, но до конца не уверен, буду ещё делать

--- lib/sprintf/parse_specifiers.c
+++ lib/sprintf/parse_specifiers.c
@@ -1,13 +1,10 @@
 #include "s21_string.h"
 
 void parse_specifier(const char *format, t_sprintf* sprintf_args , int* i){
-    char spec = '\0';
     for(int j = 0; j < s21_strlen(SPECIFIERS); j++){
-        if(format[*i] == SPECIFIERS){
-            spec = format[*i];
+        if(sprintf_args -> specifier == format[*i]){
+            sprintf_args->specifier = true;
         }
         (*i)++;
     }
-
-    return spec;
 }
