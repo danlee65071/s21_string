89b4d270ecd687c241586f941042ae98b63ba333
fedor <yatsenkolion@mail.ru>
2024-02-21 13:19:00 +0300

    n specifier

--- lib/sprintf/parse_specifiers.c
+++ lib/sprintf/parse_specifiers.c
@@ -4,6 +4,10 @@ void parse_specifier(const char *format, t_sprintf* sprintf_args , int* i){
     for(int j = 0; j < s21_strlen(SPECIFIERS); j++){
         if(sprintf_args -> specifier == format[*i]){
             sprintf_args->specifier = true;
+            if(sprintf_args -> specifier == "n"){
+                break;
+                (*i)--;
+            }
         }
         (*i)++;
     }
