c66262af7c90d3dfa578ee13d83818e287f6a08c
Marth Karyn <marthkar@mi-e5.msk.21-school.ru>
2024-02-23 19:24:20 +0300

    update

--- lib/sprintf/process_percent.c
+++ lib/sprintf/process_percent.c
@@ -5,10 +5,10 @@ void process_percent(char **str, const char *format, \
 {
     (*i)++;
     parse_flags(format, sprintf_args, i);
-    parse_width();
-    parse_precision(format, sprintf_args, i);
-    parse_length(format, sprintf_args, i);
-    parse_specifiers();
+    parse_width(format, arglist, sprintf_args, i);
+    parse_precision(format, arglist, sprintf_args, i);
+    parse_length(format, arglist, sprintf_args, i);
+    // parse_specifiers();
     // process();
 }
     
