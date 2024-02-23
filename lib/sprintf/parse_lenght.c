1d45fd6041245aed80e01d05e7fd0c6af8c3f3ea
Marth Karyn <marthkar@mi-e5.msk.21-school.ru>
2024-02-23 19:37:44 +0300

    update

--- lib/sprintf/parse_lenght.c
+++ lib/sprintf/parse_lenght.c
@@ -3,15 +3,15 @@
 void parse_length(const char *format, va_list arglist, t_sprintf* sprintf_args, int* i) {
     const char* modifiers = "lLh";
     int buf_size = 1800;
-    char *buf = calloc(1024 * sizeof(char));
+    char *buf = calloc(buf_size, sizeof(char));
 
     if (buf == NULL) {
         return;
     }
     int idx = 0;
-    while (format[i]) {
-        if (strchr(modifiers, format[i])) {
-            buf[idx] = format[i];
+    while (format[*i]) {
+        if (strchr(modifiers, format[*i])) {
+            buf[idx] = format[*i];
             break;
         }
         i++;
